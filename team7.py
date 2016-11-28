ecd2b90d55ed615708739f553cf57d09cffc7b10
QuantumPie <nnewman316@gmail.com>
2016-11-28 11:18:51 -0600

    Started algorithm

--- team7.py
+++ team7.py
@@ -7,8 +7,8 @@
 ####
 
 team_name = 'Haxosaurus Rex' # Only 10 chars displayed.
-strategy_name = 'The name the team gives to this strategy'
-strategy_description = 'How does this strategy decide?'
+strategy_name = 'The best strat'
+strategy_description = 'Betray the first 3 turns and after that Collude unless the other person has betrayed'
     
 def move(my_history, their_history, my_score, their_score):
     ''' Arguments accepted: my_history, their_history are strings.
@@ -18,6 +18,16 @@ def move(my_history, their_history, my_score, their_score):
     Returns 'c' or 'b'. 
     '''
 
+    times_colluded = 0
+    has_betrayed = False
+    if len(my_history) >= 3 and has_betrayed is False:
+        for x in range(len(my_history)):
+            if their_history[x] is 'b':
+                has_betrayed = True
+	if has_betrayed == False: 
+            return 'c'
+    return 'b'
+	       
     # my_history: a string with one letter (c or b) per round that has been played with this opponent.
     # their_history: a string of the same length as history, possibly empty. 
     # The first round between these two players is my_history[0] and their_history[0].
@@ -26,7 +36,8 @@ def move(my_history, their_history, my_score, their_score):
     # Analyze my_history and their_history and/or my_score and their_score.
     # Decide whether to return 'c' or 'b'.
     
-    return 'c'
+	#
+    return 'b'
 
     
 def test_move(my_history, their_history, my_score, their_score, result):
@@ -48,6 +59,8 @@ def test_move(my_history, their_history, my_score, their_score, result):
 if __name__ == '__main__':
      
     # Test 1: Betray on first move.
+    print test_move(my_history='bbbb', their_history='cccc', my_score=0, their_score=0, result='c')
+    
     if test_move(my_history='',
               their_history='', 
               my_score=0,
